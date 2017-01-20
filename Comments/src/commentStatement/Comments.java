7ef0f32d6f717a547a0d778f7f44613c4298088f
Chasek99 <ck121642@students.davenport.k12.ia.us>
2017-01-20 14:58:45 -0600

    Add files via upload

---
+++ Comments/src/commentStatement/Comments.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package commentStatement;
+
+/**
+ * @author CK121642
+ *
+ */
+public class Comments {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Program comments are nonexecuting statements you add to a file for the purpose of documentation");
+	}
+
+}
